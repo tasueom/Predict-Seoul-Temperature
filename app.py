b7e88671d900ae3b88618724d67700d8d1f8d584
tasueom <tasueom@naver.com>
2025-11-12 16:26:23 +0900

    feat: 작년 11월 서울시 기온 학습

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
+from sklearn.linear_model import LinearRegression
+import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
+
+model = LinearRegression()
+df = pd.read_csv('2024년11월_서울시기온.csv')
+df = df.dropna(inplace=False).reset_index(drop=True)
+model.fit(df[['일시']], df['평균기온'])
 
 @app.route('/')
 def index():
